3b0973c1339da98fdbe4a4518b1b41faf6eea66f
abdirahman buryar <buryar313@gmail.com>
2020-05-09 04:01:56 -0700

    Updates

---
+++ precache-manifest.6f8964556faa9d62348b3eb903b3229b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "765a40f87455c89f0492",
+    "url": "/crwn-clothing_01/static/css/main.4484af7c.chunk.css"
+  },
+  {
+    "revision": "765a40f87455c89f0492",
+    "url": "/crwn-clothing_01/static/js/main.13dddd97.chunk.js"
+  },
+  {
+    "revision": "581935ccd54e90d8592b",
+    "url": "/crwn-clothing_01/static/js/runtime~main.caf66481.js"
+  },
+  {
+    "revision": "503514aa8efbb2e971a9",
+    "url": "/crwn-clothing_01/static/js/2.5862941d.chunk.js"
+  },
+  {
+    "revision": "35c777724d21ee8c43af5fad5daffba5",
+    "url": "/crwn-clothing_01/index.html"
+  }
+];
